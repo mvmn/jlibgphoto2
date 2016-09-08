0b4f2c417aee4816daf64e821d697c92eab1c014
Mykola Makhin <Mykola.Makhin@globallogic.com>
2016-09-08 22:05:21 +0300

    Finished logic for obtaining config.

---
+++ src/main/java/x/mvmn/jlibgphoto2/CameraConfigEntryBean.java
@@ -0,0 +1,264 @@
+package x.mvmn.jlibgphoto2;
+
+import java.util.Arrays;
+
+import x.mvmn.gphoto2.jna.Gphoto2Library.CameraWidgetType;
+
+public class CameraConfigEntryBean {
+
+	public static enum CameraConfigEntryValueType {
+		STRING, INT, FLOAT;
+	}
+
+	public static enum CameraConfigEntryType {
+		TEXT(CameraWidgetType.GP_WIDGET_TEXT, CameraConfigEntryValueType.STRING), RANGE(CameraWidgetType.GP_WIDGET_RANGE,
+				CameraConfigEntryValueType.FLOAT), TOGGLE(CameraWidgetType.GP_WIDGET_TOGGLE, CameraConfigEntryValueType.INT), RADIO(
+						CameraWidgetType.GP_WIDGET_RADIO, CameraConfigEntryValueType.STRING), MENU(CameraWidgetType.GP_WIDGET_MENU,
+								CameraConfigEntryValueType.STRING), DATE(CameraWidgetType.GP_WIDGET_DATE, CameraConfigEntryValueType.INT);
+
+		private final int code;
+		private final CameraConfigEntryValueType valueType;
+
+		private CameraConfigEntryType(final int code, final CameraConfigEntryValueType valueType) {
+			this.code = code;
+			this.valueType = valueType;
+		}
+
+		public int getCode() {
+			return code;
+		}
+
+		public CameraConfigEntryValueType getValueType() {
+			return valueType;
+		}
+
+		public static CameraConfigEntryType getByCode(int code) {
+			CameraConfigEntryType result = null;
+			for (CameraConfigEntryType type : CameraConfigEntryType.values()) {
+				if (type.getCode() == code) {
+					result = type;
+					break;
+				}
+			}
+			return result;
+		}
+	}
+
+	public static class Range {
+		protected final float min;
+		protected final float max;
+		protected final float step;
+
+		public Range(float min, float max, float step) {
+			super();
+			this.min = min;
+			this.max = max;
+			this.step = step;
+		}
+
+		public float getMin() {
+			return min;
+		}
+
+		public float getMax() {
+			return max;
+		}
+
+		public float getStep() {
+			return step;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("Range [min=").append(min).append(", max=").append(max).append(", step=").append(step).append("]");
+			return builder.toString();
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + Float.floatToIntBits(max);
+			result = prime * result + Float.floatToIntBits(min);
+			result = prime * result + Float.floatToIntBits(step);
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			CameraConfigEntryBean.Range other = (CameraConfigEntryBean.Range) obj;
+			if (Float.floatToIntBits(max) != Float.floatToIntBits(other.max))
+				return false;
+			if (Float.floatToIntBits(min) != Float.floatToIntBits(other.min))
+				return false;
+			if (Float.floatToIntBits(step) != Float.floatToIntBits(other.step))
+				return false;
+			return true;
+		}
+	}
+
+	protected final int id;
+	protected final String path;
+	protected final String label;
+	protected final String info;
+	protected final CameraConfigEntryType type;
+	protected final Integer intValue;
+	protected final Float floatValue;
+	protected final String strValue;
+	protected final CameraConfigEntryBean.Range range;
+	protected final String[] choices;
+
+	public CameraConfigEntryBean(int id, String path, String label, String info, CameraConfigEntryType type, Integer intValue, Float floatValue,
+			String strValue, CameraConfigEntryBean.Range range, String[] choices) {
+		super();
+		this.id = id;
+		this.path = path;
+		this.label = label;
+		this.info = info;
+		this.type = type;
+		this.intValue = intValue;
+		this.floatValue = floatValue;
+		this.strValue = strValue;
+		this.range = range;
+		this.choices = choices;
+	}
+
+	public CameraConfigEntryBean cloneWithNewValue(String strValue) {
+		return new CameraConfigEntryBean(id, path, label, info, type, intValue, floatValue, strValue, range, choices);
+	}
+
+	public CameraConfigEntryBean cloneWithNewValue(int intValue) {
+		return new CameraConfigEntryBean(id, path, label, info, type, intValue, floatValue, strValue, range, choices);
+	}
+
+	public CameraConfigEntryBean cloneWithNewValue(float floatValue) {
+		return new CameraConfigEntryBean(id, path, label, info, type, intValue, floatValue, strValue, range, choices);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public CameraConfigEntryType getType() {
+		return type;
+	}
+
+	public Integer getIntValue() {
+		return intValue;
+	}
+
+	public Float getFloatValue() {
+		return floatValue;
+	}
+
+	public String getStrValue() {
+		return strValue;
+	}
+
+	public CameraConfigEntryBean.Range getRange() {
+		return range;
+	}
+
+	public String[] getChoices() {
+		return choices;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(choices);
+		result = prime * result + ((floatValue == null) ? 0 : floatValue.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((info == null) ? 0 : info.hashCode());
+		result = prime * result + ((intValue == null) ? 0 : intValue.hashCode());
+		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		result = prime * result + ((range == null) ? 0 : range.hashCode());
+		result = prime * result + ((strValue == null) ? 0 : strValue.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CameraConfigEntryBean other = (CameraConfigEntryBean) obj;
+		if (!Arrays.equals(choices, other.choices))
+			return false;
+		if (floatValue == null) {
+			if (other.floatValue != null)
+				return false;
+		} else if (!floatValue.equals(other.floatValue))
+			return false;
+		if (id != other.id)
+			return false;
+		if (info == null) {
+			if (other.info != null)
+				return false;
+		} else if (!info.equals(other.info))
+			return false;
+		if (intValue == null) {
+			if (other.intValue != null)
+				return false;
+		} else if (!intValue.equals(other.intValue))
+			return false;
+		if (label == null) {
+			if (other.label != null)
+				return false;
+		} else if (!label.equals(other.label))
+			return false;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		if (range == null) {
+			if (other.range != null)
+				return false;
+		} else if (!range.equals(other.range))
+			return false;
+		if (strValue == null) {
+			if (other.strValue != null)
+				return false;
+		} else if (!strValue.equals(other.strValue))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("CameraConfigEntryBean [id=").append(id).append(", path=").append(path).append(", label=").append(label).append(", info=").append(info)
+				.append(", type=").append(type).append(", intValue=").append(intValue).append(", floatValue=").append(floatValue).append(", strValue=")
+				.append(strValue).append(", range=").append(range).append(", choices=").append(Arrays.toString(choices)).append("]");
+		return builder.toString();
+	}
+
+}
--- src/main/java/x/mvmn/jlibgphoto2/GP2ConfigHelper.java
+++ src/main/java/x/mvmn/jlibgphoto2/GP2ConfigHelper.java
@@ -2,59 +2,36 @@ package x.mvmn.jlibgphoto2;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.sun.jna.ptr.FloatByReference;
 import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.PointerByReference;
 
 import x.mvmn.gphoto2.jna.Gphoto2Library;
-import x.mvmn.gphoto2.jna.Gphoto2Library.CameraWidgetType;
+import x.mvmn.jlibgphoto2.CameraConfigEntryBean.CameraConfigEntryType;
+import x.mvmn.jlibgphoto2.CameraConfigEntryBean.Range;
 import x.mvmn.jlibgphoto2.util.GP2ErrorHelper;
 
 public class GP2ConfigHelper {
 
 	public static void main(String args[]) throws Exception {
 		GP2Camera camera = new GP2Camera();
-		getConfig(camera);
+		for (CameraConfigEntryBean configEntry : getConfig(camera)) {
+			System.out.println(configEntry);
+		}
 		camera.close();
 	}
 
-	protected static enum GP2CameraWidgetType {
-		GP_WIDGET_WINDOW(CameraWidgetType.GP_WIDGET_WINDOW), GP_WIDGET_SECTION(CameraWidgetType.GP_WIDGET_SECTION), GP_WIDGET_TEXT(
-				CameraWidgetType.GP_WIDGET_TEXT), GP_WIDGET_RANGE(CameraWidgetType.GP_WIDGET_RANGE), GP_WIDGET_TOGGLE(
-						CameraWidgetType.GP_WIDGET_TOGGLE), GP_WIDGET_RADIO(CameraWidgetType.GP_WIDGET_RADIO), GP_WIDGET_MENU(
-								CameraWidgetType.GP_WIDGET_MENU), GP_WIDGET_BUTTON(
-										CameraWidgetType.GP_WIDGET_BUTTON), GP_WIDGET_DATE(CameraWidgetType.GP_WIDGET_DATE);
-
-		private final int code;
-
-		private GP2CameraWidgetType(int code) {
-			this.code = code;
-		}
-
-		public int getCode() {
-			return code;
-		}
-
-		public static GP2CameraWidgetType getByCode(int code) {
-			GP2CameraWidgetType result = null;
-			for (GP2CameraWidgetType type : GP2CameraWidgetType.values()) {
-				if (type.getCode() == code) {
-					result = type;
-					break;
-				}
-			}
-			return result;
-		}
-	}
-
-	public static void getConfig(GP2Camera camera) {
+	public static List<CameraConfigEntryBean> getConfig(GP2Camera camera) {
+		List<CameraConfigEntryBean> result = new ArrayList<CameraConfigEntryBean>();
 		PointerByReference cameraWidget = new PointerByReference();
 		try {
 			GP2ErrorHelper.checkResult(
 					Gphoto2Library.INSTANCE.gp_camera_get_config(camera.getCameraByReference(), cameraWidget, camera.getContext().getPointerByRef()));
 			cameraWidget.setPointer(cameraWidget.getValue());
-			visitWidgets(cameraWidget, "");
+			visitWidgets(cameraWidget, "", result);
 		} catch (Exception e) {
 			if (e instanceof RuntimeException) {
 				throw (RuntimeException) e;
@@ -68,14 +45,17 @@ public class GP2ConfigHelper {
 				ex.printStackTrace();
 			}
 		}
+		return result;
 	}
 
-	protected static void visitWidgets(PointerByReference parentWidget, String parentPath) {
+	protected static void visitWidgets(PointerByReference parentWidget, String parentPath, List<CameraConfigEntryBean> result) {
 		final int widgetCountChildren = GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_count_children(parentWidget));
 
 		final PointerByReference pbrWidgetName = new PointerByReference();
 		GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_name(parentWidget, pbrWidgetName));
 		final String widgetName = pbrWidgetName.getValue().getString(0);
+
+		final String widgetPath = parentPath + "/" + widgetName;
 
 		final PointerByReference pbrWidgetLabel = new PointerByReference();
 		GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_label(parentWidget, pbrWidgetLabel));
@@ -91,7 +71,7 @@ public class GP2ConfigHelper {
 			GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_type(parentWidget, intBuff));
 			widgetTypeCode = intBuff.get();
 		}
-		final GP2CameraWidgetType widgetType = GP2CameraWidgetType.getByCode(widgetTypeCode);
+		final CameraConfigEntryType configEntryType = CameraConfigEntryType.getByCode(widgetTypeCode);
 
 		final int widgetId;
 		{
@@ -100,54 +80,58 @@ public class GP2ConfigHelper {
 			widgetId = intBuff.get();
 		}
 
-		System.out.println(widgetId + " - " + widgetLabel + " (" + widgetInfo + "): " + parentPath + "/" + widgetName + " " + widgetType);
+		Range range = null;
+		String[] choices = null;
 
-		if (widgetType.equals(GP2CameraWidgetType.GP_WIDGET_RANGE)) {
-			final FloatBuffer fbMin = FloatBuffer.allocate(1);
-			final FloatBuffer fbMax = FloatBuffer.allocate(1);
-			final FloatBuffer fbStep = FloatBuffer.allocate(1);
-			GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_range(parentWidget, fbMin, fbMax, fbStep));
-			ConfigPropertyRangeBean range = new ConfigPropertyRangeBean(fbMin.get(), fbMax.get(), fbStep.get());
-			System.out.println(" - Range: " + range);
-		}
-		if (widgetType.equals(GP2CameraWidgetType.GP_WIDGET_RADIO) || widgetType.equals(GP2CameraWidgetType.GP_WIDGET_MENU)) {
-			int choicesCount = GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_count_choices(parentWidget));
-			for (int i = 0; i < choicesCount; i++) {
-				PointerByReference pbrChoice = new PointerByReference();
-				GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_choice(parentWidget, i, pbrChoice));
-				System.out.println(" - Choice: " + pbrChoice.getValue().getString(0));
+		if (configEntryType != null) {
+			if (configEntryType.equals(CameraConfigEntryType.RANGE)) {
+				final FloatBuffer fbMin = FloatBuffer.allocate(1);
+				final FloatBuffer fbMax = FloatBuffer.allocate(1);
+				final FloatBuffer fbStep = FloatBuffer.allocate(1);
+				GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_range(parentWidget, fbMin, fbMax, fbStep));
+				range = new Range(fbMin.get(), fbMax.get(), fbStep.get());
+			}
+			if (configEntryType.equals(CameraConfigEntryType.RADIO) || configEntryType.equals(CameraConfigEntryType.MENU)) {
+				int choicesCount = GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_count_choices(parentWidget));
+				choices = new String[choicesCount];
+				for (int i = 0; i < choicesCount; i++) {
+					PointerByReference pbrChoice = new PointerByReference();
+					GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_choice(parentWidget, i, pbrChoice));
+					choices[i] = pbrChoice.getValue().getString(0);
+				}
 			}
 		}
-		if (!widgetType.equals(GP2CameraWidgetType.GP_WIDGET_WINDOW) && !widgetType.equals(GP2CameraWidgetType.GP_WIDGET_SECTION)
-				&& !widgetType.equals(GP2CameraWidgetType.GP_WIDGET_BUTTON)) {
-			switch (widgetType) {
-				case GP_WIDGET_MENU:
-				case GP_WIDGET_RADIO:
-				case GP_WIDGET_TEXT:
-					PointerByReference pbr = new PointerByReference();
-					GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_value(parentWidget, pbr));
-					System.out.println("! Value == " + (pbr.getValue() != null ? pbr.getValue().getString(0) : null));
+		String strValue = null;
+		Float floatValue = null;
+		Integer intValue = null;
+		if (configEntryType != null) {
+			switch (configEntryType.getValueType()) {
+				case STRING:
+					PointerByReference byRefPointer = new PointerByReference();
+					GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_value(parentWidget, byRefPointer));
+					strValue = byRefPointer.getValue() != null ? byRefPointer.getValue().getString(0) : ""; // Not sure if null-check is necessary
 				break;
-				case GP_WIDGET_RANGE:
-					FloatByReference fbr = new FloatByReference();
-					GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_value(parentWidget, fbr));
-					System.out.println("! Value == " + fbr.getValue());
+				case FLOAT:
+					FloatByReference byRefFloat = new FloatByReference();
+					GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_value(parentWidget, byRefFloat));
+					floatValue = byRefFloat.getValue();
 				break;
-				case GP_WIDGET_TOGGLE:
-				case GP_WIDGET_DATE:
-					IntByReference ibr = new IntByReference();
-					GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_value(parentWidget, ibr));
-					System.out.println("! Value == " + ibr.getValue());
+				case INT:
+					IntByReference byRefInteger = new IntByReference();
+					GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_value(parentWidget, byRefInteger));
+					intValue = byRefInteger.getValue();
 				break;
 				default:
 			}
+			result.add(
+					new CameraConfigEntryBean(widgetId, widgetPath, widgetLabel, widgetInfo, configEntryType, intValue, floatValue, strValue, range, choices));
 		}
 
 		for (int i = 0; i < widgetCountChildren; i++) {
 			final PointerByReference pbrChildWidget = new PointerByReference();
 			GP2ErrorHelper.checkResult(Gphoto2Library.INSTANCE.gp_widget_get_child(parentWidget, i, pbrChildWidget));
 			pbrChildWidget.setPointer(pbrChildWidget.getValue());
-			visitWidgets(pbrChildWidget, parentPath + "/" + widgetName);
+			visitWidgets(pbrChildWidget, parentPath + "/" + widgetName, result);
 		}
 	}
 }
